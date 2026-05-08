609c2d27ae9d82623b403e869b598d80afd811ea
Aethelgard Dev <developer@aethelgard.dev>
2026-05-08 14:56:36 +0800

    fix: 更新 tsx 瓦片集图片路径
    - tiles.tsx: tiles.png
    - tiles_v2.tsx: tiles_v2.png

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="tiles" tilewidth="185" tileheight="190" spacing="60" tilecount="6" columns="5">
- <image source="../TraeWorker/assets/tiles.png" width="1169" height="198"/>
+ <image source="tiles.png" width="1169" height="198"/>
 </tileset>
